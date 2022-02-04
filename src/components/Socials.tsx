7874b0e74f793c4eae55d0de6629f704db55085e
Karel van Klink <karel@vank.link>
2022-02-04 18:49:52 +0100

    Update socials
    Remove link to Instagram

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { FaLinkedinIn, FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 import { FiGithub } from "react-icons/fi";
 import "./socials.css";
 
@@ -20,14 +20,6 @@ const Socials = () => (
       rel="noopener noreferrer"
     >
       <FaLinkedinIn className="icon" />
-    </a>
-    <a
-      href="https://instagram.com/karelorkyle"
-      title="Instagram"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <FaInstagram className="icon" />
     </a>
   </div>
 );
